0710fac80c2eaf2a3eb29382ac0acdf9cca8b87e
Jackson Cannon <eviljackson1@gmail.com>
2018-02-24 16:07:07 -0800

    Update fedora.py

--- fedora/fedora.py
+++ fedora/fedora.py
@@ -14,28 +14,37 @@ class Fedora:
 
     @commands.command(pass_context=True,no_pm=True)
     async def image(self, ctx, *text):
-        """Retrieves the latest result from Derpibooru"""
-        await fetch_image(self, ctx, text)
+        await fetch_image(self, ctx, text[0])
 
     @commands.command(pass_context=True,no_pm=True)
     async def gif(self, ctx, *text):
-        """Retrieves a random result from Derpibooru"""
-        await fetch_image(self, ctx, "gif "+text)
+        await fetch_image(self, ctx, "gif "+(text[0]))
 
 async def fetch_image(self, ctx, text):
     server = ctx.message.server
 
     # Fetch the image or display an error
     try:
-        async with aiohttp.request('post','https://swampservers.net/fedorabot/', params={'q':text}) as r:
+        async with aiohttp.request('post','https://swampservers.net/fedorabot/', data={'q':text}) as r:
             website = await r.text()
 
         url = website.split('url:')
         if len(url) > 1:
+            safe = False
+            if url[0].endswith("safe"):
+                safe = True
+            channame = ctx.message.channel.name.lower()
+            if "nsfw" in channame or channame=="straight" or channame=="gay" or channame=="anything":
+                safe = True
+
+            if not safe:
+                return await self.bot.say("NSFW result, go to an NSFW channel")
+
+            numba = url[0].split(")")[0]+")"
             url = url[-1][:-1]
-            return await self.bot.say(url)
+            return await self.bot.say(numba+" "+url)
         else:
-            return await self.bot.say("Your search terms gave no results.")
+            return await self.bot.say("No results.")
 
     except:
         return await self.bot.say("Error.")
